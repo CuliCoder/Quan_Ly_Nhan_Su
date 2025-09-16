0495efa3c397e52b6833d56cfc2ce27afc155ffd
trungnc113 <nguyencongtrung13@gmail.com>
2025-09-16 10:28:45 +0700

    create connect db prepare for DAO,BUS

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using Quan_Ly_Nhan_Su.config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_Ly_Nhan_Su
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            connectDB connectDB = new connectDB();
+            MySqlConnection con = connectDB.getConnection();
+            con.Open();
+            string query = "select * from user";
+            MySqlCommand mySqlCommand = new MySqlCommand(query, con);
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(1));
+            }
+        }
+    }
+}
