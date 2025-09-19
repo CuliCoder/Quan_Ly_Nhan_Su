3970a39f357634b0a85bc22d84036490eecf65db
trungnc113 <nguyencongtrung13@gmail.com>
2025-09-19 12:08:28 +0700

    main gui

---
+++ config/design.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZstdSharp.Unsafe;
+
+namespace Quan_Ly_Nhan_Su.config
+{
+    internal class design
+    {
+        public static void paintBorder(Panel pn ,Color color,float width, int x1,int y1,int x2,int y2 )
+        {
+            pn.Paint += (Object sender, PaintEventArgs e) =>
+            {
+                Pen pen = new Pen(color, width);
+                e.Graphics.DrawLine(pen,new Point(x1,y1), new Point(x2,y2));
+            };
+        }
+    }
+}
