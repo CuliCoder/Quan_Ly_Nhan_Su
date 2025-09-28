919a25d007734cf8f6958a9fd1f27e1e94e50185
mag1214 <thienbaoqn2004@gmail.com>
2025-09-28 15:43:44 +0700

    GUI danh gia va luong thuong

---
+++ GUI/DanhGiaUserControl/DanhGia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_Ly_Nhan_Su.GUI.DanhGiaUserControl
+{
+    public partial class DanhGia : UserControl
+    {
+        public DanhGia()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
