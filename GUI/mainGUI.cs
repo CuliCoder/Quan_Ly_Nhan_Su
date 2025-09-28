a85a130866917b6ba693ebbb66b702e9c82b45af
trungnc113 <nguyencongtrung13@gmail.com>
2025-09-28 23:38:48 +0700

    add usercontrol homepage into main gui

--- GUI/mainGUI.cs
+++ GUI/mainGUI.cs
@@ -13,16 +13,24 @@ namespace Quan_Ly_Nhan_Su.GUI
 {
     public partial class mainGUI : Form
     {
+        homePage homePage = new homePage();
+
         public mainGUI()
         {
             InitializeComponent();
             designForm();
-            pnlbTrangChu_Click(pnlbTrangChu, EventArgs.Empty);
+            addUserControl();
         }
         private void designForm()
         {
             design.paintBorder(this.panel5, Color.Gray, 4, 30, 0, this.panel5.Width - 30, 0);
             design.paintBorder(this.panel2, Color.Gray, 4, 0, 0, 0, this.panel2.Height);
+        }
+        private void addUserControl()
+        {
+            homePage.Dock = DockStyle.Fill;
+            this.panel6.Controls.Add(homePage);
+            homePage.BringToFront();
         }
         private void mainGUI_Load(object sender, EventArgs e)
         {
@@ -81,20 +89,12 @@ namespace Quan_Ly_Nhan_Su.GUI
 
         private void pnlbTrangChu_Click(object sender, EventArgs e)
         {
-            bodyContent.Controls.Clear();
-            homePage home = new homePage();
-            home.Dock = DockStyle.Fill;
-            bodyContent.Controls.Add(home);
-            pnlbTrangChu.BackColor = System.Drawing.ColorTranslator.FromHtml("#5DC2A7");
+           
         }
 
         private void pnlbPhongBan_Click(object sender, EventArgs e)
         {
-            bodyContent.Controls.Clear();
-            departmentTab department = new departmentTab();
-            department.Dock = DockStyle.Fill;
-            bodyContent.Controls.Add(department);
-            pnlbPhongBan.BackColor = System.Drawing.ColorTranslator.FromHtml("#5DC2A7");
+            
         }
     }
 }
