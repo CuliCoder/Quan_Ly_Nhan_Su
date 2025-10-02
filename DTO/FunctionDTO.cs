946e2e5217519eafd5f811465323561cbe2087c6
mag1214 <thienbaoqn2004@gmail.com>
2025-10-02 13:53:36 +0700

    dto, dao, bus chuc nang va ho so ca nhan

---
+++ DTO/FunctionDTO.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Quan_Ly_Nhan_Su.DTO
+{
+  /// <summary>
+  /// DTO for Function table
+  /// </summary>
+  public class FunctionDTO
+  {
+    public string MaChucNang { get; set; }
+    public string TenChucNang { get; set; }
+    public string TinhTrang { get; set; }
+  }
+}
