4bc7bf1fb4d2c69ef13afc1ec4d41c5141bfe8c1
mag1214 <thienbaoqn2004@gmail.com>
2025-11-17 15:06:07 +0700

    fix loi DAO

--- DAO/SalaryFullDAO.cs
+++ DAO/SalaryFullDAO.cs
@@ -19,7 +19,7 @@ namespace Quan_Ly_Nhan_Su.DAO
                     IFNULL(SUM(CASE WHEN p.Loai='KhoanTru' THEN p.SoTien ELSE 0 END),0) AS TongKhoanTru,
                     IFNULL(SUM(t.PhanTramThuong),0) AS TongThuong
                 FROM nhanvien nv
-                JOIN luong l ON nv.MaLuong = l.MaLuong
+                JOIN luong l ON nv.MaNhanVien = l.MaNhanVien
                 LEFT JOIN phucapkhoantru p 
                     ON p.MaNhanVien = nv.MaNhanVien 
                     AND p.ThangApDung = @Thang 
